26799d171cc9a53f1f32d3e716914e793919071d
welintron <welintron@yahoo.com>
2017-08-11 11:50:05 -0300

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  //zoom level
+  zoom: number = 10;
+
+  //star position
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  //markers
+  markers:  marker[] = [
+    {
+      name: 'Company One',
+      lat: 51.678200,
+      lng: 7.809200,
+      draggable: true
+
+    },
+    {
+      name: 'Company Two',
+      lat: 51.679200,
+      lng: 7.819200,
+      draggable: true
+
+    },    
+    {
+      name: 'Company One',
+      lat: 51.688200,
+      lng: 7.808200,
+      draggable: true
+
+    },
+  ];
+
+  constructor(){
+
+  }
+
+  
+}
+//Market Type
+interface marker{
+  name?:string;
+  lat:number;
+  lng:number;
+  draggable:boolean;
+
 }
